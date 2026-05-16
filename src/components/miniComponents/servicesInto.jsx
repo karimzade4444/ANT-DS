80b951f204e5d195ca490e88e034714e9d675a8b
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-16 22:21:52 +0500

    ServicesInto

---
+++ src/components/miniComponents/servicesInto.jsx
@@ -0,0 +1,14 @@
+import girl from '../../assets/IMG_7433 1.png'
+
+const ServicesInto = () => {
+  return (
+    <div>
+        <p>SERVICES</p>
+   <img src={girl} alt="" />
+   <p>НАШИ КОНТАКТЫ</p>
+    <p>Наши сотрудники всегда готовы помочь вам с выбором, оформить бронь и предоставить лучший сервис</p>
+    </div>
+  )
+}
+
+export default ServicesInto
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -1,9 +1,10 @@
 import bgs from '../assets/Group 7156.png'
+import ServicesInto from './miniComponents/servicesInto'
 
 const Services = () => {
   return (
     <div style={{backgroundImage:`url(${bgs})`}} className='bg-center bg-cover h-[200vh]'>
-
+    <ServicesInto/>
     </div>
   )
 }
