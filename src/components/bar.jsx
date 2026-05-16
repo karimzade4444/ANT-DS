64566d0f00d9faeaa4b1faba24d20dbd9d93c0c8
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-16 20:41:30 +0500

    titleBar

--- src/components/bar.jsx
+++ src/components/bar.jsx
@@ -1,8 +1,16 @@
 
+import bgb from '../assets/DSC08022 3.png'
 
 const Bar = () => {
   return (
-    <div>Bar</div>
+        <div
+          style={{ backgroundImage: `url(${bgb})` }}
+          className="h-screen bg-cover bg-center relative"
+        >
+          <p className=" absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-5xl leading-15 w-40 text-center">
+            Bar & Hookah
+          </p>
+        </div>
   )
 }
 
