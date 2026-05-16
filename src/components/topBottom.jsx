bebaedf0b8a2ab7d170c96fb029ec7aed3a8455d
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-16 19:46:55 +0500

    TopBottom

--- src/components/topBottom.jsx
+++ src/components/topBottom.jsx
@@ -2,11 +2,15 @@ import bgTB from '../assets/DSC088w02 1 2.png'
 
 const TopBottom = () => {
   return (
-    <div style={{ backgroundImage: `url(${bgTB})` }}
-          className="h-screen bg-cover bg-center">
-
+    <div
+      style={{ backgroundImage: `url(${bgTB})` }}
+      className="h-screen bg-cover bg-center relative"
+    >
+      <p className=" absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-4xl" >
+        About us
+      </p>
     </div>
-  )
+  );
 }
 
 export default TopBottom
