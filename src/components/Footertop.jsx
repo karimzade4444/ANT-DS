27abe359f58b228fa0450a04a2b117e92b617cc6
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-17 00:06:59 +0500

    footer

--- src/components/Footertop.jsx
+++ src/components/Footertop.jsx
@@ -3,8 +3,8 @@ import location from '../assets/Screenshot2_13 1 2.png'
 import FooterBottom from './miniComponents/footerBottom'
 const Footertop = () => {
   return (
-    <div className='h-screen'>
-<img src={location} alt=""  className='w-full'/>
+    <div >
+<img src={location} alt=""  className='w-full h-screen object-cover '/>
 <FooterBottom/>
     </div>
   )
--- src/components/miniComponents/footerBottom.jsx
+++ src/components/miniComponents/footerBottom.jsx
@@ -1,8 +1,18 @@
+import logotype from '../../assets/Group 7169.png'
 
 
 const FooterBottom = () => {
   return (
-    <div>footerBottom</div>
+    <div className="h-screen bg-black/95 pt-20">
+<div className=' flex justify-center items-center flex-col leading-20 text-white text-3xl'>
+    <img src={logotype} alt=""  className='w-50'/>
+    <p className='pt-10'>About us</p>
+    <p>Your favorite places</p>
+    <p>Bar & Hookah</p>
+    <p>Services</p>
+</div>
+<p className='text-white/40 text-center pt-30 text-2xl'>@2022-2026 Gosu Cybersoft - Больше, чем просто компьютерный клуб</p>
+    </div>
   )
 }
 
