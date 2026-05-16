1d21749278f8f6350c1afc5e2d072b77cc36dee4
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-16 21:51:34 +0500

    disEnd

--- src/components/discount.jsx
+++ src/components/discount.jsx
@@ -7,7 +7,7 @@ const Discount = () => {
               className="h-screen bg-cover bg-center relative"
             >
               <p className=" absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-5xl leading-15 w-40 text-center">
-                Bar & Hookah
+                Discount
               </p>
             </div>
   )
