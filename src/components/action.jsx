acf2f658cb918196ce46521bdc6add8efcecf9cd
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-16 22:08:57 +0500

    Action

--- src/components/action.jsx
+++ src/components/action.jsx
@@ -1,8 +1,11 @@
-
+import bga from '../assets/Group 7157.png'
+import ActionInto from './miniComponents/actionInto'
 
 const Action = () => {
   return (
-    <div>action</div>
+    <div style={{ backgroundImage: `url(${bga})` }} className="h-[120vh] bg-cover bg-center relative">
+        <ActionInto/>
+    </div>
   )
 }
 
---
+++ src/components/miniComponents/actionInto.jsx
@@ -0,0 +1,13 @@
+import wook from '../../assets/DSC02819 1.png'
+
+const ActionInto = () => {
+  return (
+    <div className='p-5'>
+   <p className='text-green-600 font-black text-4xl text-center pt-10'>Акции</p>
+   <img src={wook} alt=""  className=' w-full pt-20'/>
+   <p className='text-green-600 text-3xl pt-10'>ПРИ ПОКУПКЕ ПАРОВОГО КОКТЕЙЛЯ ЧАС ИГРЫ В ПОДАРОК</p>
+    </div>
+  )
+}
+
+export default ActionInto
