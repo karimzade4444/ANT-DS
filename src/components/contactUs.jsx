e0cedce219dfdd6724acecd7652eb7749129a26d
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-16 22:13:16 +0500

    CU

--- src/components/contactUs.jsx
+++ src/components/contactUs.jsx
@@ -1,8 +1,13 @@
-
+import cu from '../assets/DSC08022 1.png'
 
 const ContactUs = () => {
   return (
-    <div>contactUs</div>
+     <div style={{ backgroundImage: `url(${cu})` }} className="h-screen bg-cover bg-center relative"
+            >
+              <p className=" absolute text-white top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  text-5xl   text-center">
+                Contact us
+              </p>
+            </div>
   )
 }
 
