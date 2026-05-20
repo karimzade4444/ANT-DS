ee767473f2d1427e02a46b5767f41cc1815d2412
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-20 21:29:20 +0500

    adaptive

--- src/components/middleTop.jsx
+++ src/components/middleTop.jsx
@@ -1,5 +1,6 @@
 import bg from '../assets/Group 7159.png'
 import MiddleTopInto from './miniComponents/middleTopInto'
+import keyboard from '../assets/Девайсы premium брендов (посмотри какая из двух лучше) 1.png'
 
 const MiddleTop = () => {
   return (
@@ -8,7 +9,10 @@ const MiddleTop = () => {
         style={{ backgroundImage: `url(${bg})` }}
         className="h-screen bg-cover bg-center lg:hidden"
       >
-        
+        <div>
+          <p>ABOUT US</p>
+          <img src={keyboard} alt="" />
+        </div>
       </div>
       <div
         className="hidden h-screen bg-cover bg-center lg:block bg-black/95"
