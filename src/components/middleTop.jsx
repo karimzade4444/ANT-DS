9d97507ef9d5c945d67e95fcce44f3160ee199c9
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-20 22:06:09 +0500

    bottom

--- src/components/middleTop.jsx
+++ src/components/middleTop.jsx
@@ -9,14 +9,21 @@ const MiddleTop = () => {
         style={{ backgroundImage: `url(${bg})` }}
         className="h-screen bg-cover bg-center lg:hidden"
       >
-        <div>
-          <p>ABOUT US</p>
-          <img src={keyboard} alt="" />
+        <div className='p-5'>
+          <p className='text-green-600 text-3xl text-center pt-10'>ABOUT US</p>
+          <img src={keyboard} alt=""  className='w-full pt-10'/>
+          <p className="text-green-600  text-3xl pt-5 lg:text-6xl">
+            FPS ИМЕЕТ ЗНАЧЕНИЕ
+          </p>
+          <p className="text-white pt-5 text-xl lg:text-xl lg:w-145">
+            Компьютерный клуб построенный геймерами для геймеров. Каждая из
+            наших игровых зон отвечает требованиям любых современных игровых
+            проектов, позволяет добиваться высоких результатов и обходить
+            соперников.
+          </p>
         </div>
       </div>
-      <div
-        className="hidden h-screen bg-cover bg-center lg:block bg-black/95"
-      >
+      <div className="hidden h-screen bg-cover bg-center lg:block bg-black/95">
         <MiddleTopInto />
       </div>
     </>
