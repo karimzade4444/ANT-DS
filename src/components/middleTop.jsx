40a35d79dcb7aee864fc61733ec12f41ead97272
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-20 19:01:04 +0500

    swipe

--- src/components/middleTop.jsx
+++ src/components/middleTop.jsx
@@ -3,11 +3,20 @@ import MiddleTopInto from './miniComponents/middleTopInto'
 
 const MiddleTop = () => {
   return (
-    <div style={{ backgroundImage: `url(${bg})` }}
-          className="h-screen bg-cover bg-center">
-            <MiddleTopInto/>
-          </div>
-  )
+    <>
+      <div
+        style={{ backgroundImage: `url(${bg})` }}
+        className="h-screen bg-cover bg-center lg:hidden"
+      >
+        <MiddleTopInto />
+      </div>
+      <div
+        className="hidden h-screen bg-cover bg-center lg:block bg-black/95"
+      >
+        <MiddleTopInto />
+      </div>
+    </>
+  );
 }
 
 export default MiddleTop
--- src/components/miniComponents/middleTopInto.jsx
+++ src/components/miniComponents/middleTopInto.jsx
@@ -12,42 +12,55 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
+import bg1 from '../../assets/Group 71.png'
 
 
 const MiddleTopInto = () => {
   return (
     <div className="p-5 lg:p-0">
-      <p className="text-green-600 font-bold text-3xl text-center pt-10">
+      <p className="text-green-600 font-bold text-3xl text-center pt-10 lg:text-start lg:text-xl lg:pl-30">
         ABOUT US
       </p>
-      <Swiper
-        slidesPerView={1}
-        spaceBetween={100}
-        onSlideChange={() => console.log("slide change")}
-        onSwiper={(swiper) => console.log(swiper)}
-        modules={[Pagination, Autoplay]}
-        pagination={{ clickable: true }}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
-        className="hidden! lg:block!"
-      >
-        <SwiperSlide>
-          <div className="w-full h-50 bg-blue-600"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full h-50 bg-red-600"></div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="w-full h-50 bg-green-600"></div>
-        </SwiperSlide>
-      </Swiper>
+      <div className="lg:flex lg:flex-row-reverse lg:justify-center lg:items-center lg:relative">
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={50}
+          modules={[Pagination, Autoplay]}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 3000, disableOnInteraction: false }}
+          className="w-full lg:w-[600px]"
+        >
+          <SwiperSlide>
+            <div className="w-full flex justify-center">
+              <img src={keyboardImg} alt="" className="w-full max-w-[600px]" />
+            </div>
+          </SwiperSlide>
 
-      <img src={keyboardImg} alt="" className="w-full pt-20 lg:hidden" />
-      <p className="text-green-600  text-3xl pt-5">FPS ИМЕЕТ ЗНАЧЕНИЕ</p>
-      <p className="text-white pt-5 text-xl">
-        Компьютерный клуб построенный геймерами для геймеров. Каждая из наших
-        игровых зон отвечает требованиям любых современных игровых проектов,
-        позволяет добиваться высоких результатов и обходить соперников.
-      </p>
+          <SwiperSlide>
+            <div className="w-full flex justify-center">
+              <img src={keyboardImg} alt="" className="w-full max-w-[600px]" />
+            </div>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <div className="w-full flex justify-center">
+              <img src={keyboardImg} alt="" className="w-full max-w-[600px]" />
+            </div>
+          </SwiperSlide>
+        </Swiper>
+        <div className=" lg:absolute lg:z-10 lg:left-30">
+          <img src={keyboardImg} alt="" className="w-full pt-20 lg:hidden" />
+          <p className="text-green-600  text-3xl pt-5 lg:text-6xl">
+            FPS ИМЕЕТ ЗНАЧЕНИЕ
+          </p>
+          <p className="text-white pt-5 text-xl lg:text-xl lg:w-145">
+            Компьютерный клуб построенный геймерами для геймеров. Каждая из
+            наших игровых зон отвечает требованиям любых современных игровых
+            проектов, позволяет добиваться высоких результатов и обходить
+            соперников.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
