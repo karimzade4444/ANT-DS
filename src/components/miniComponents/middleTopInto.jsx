b84b1c8189a1d1006f9f4cbe81e87a9acb4f90a8
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-20 20:05:39 +0500

    1 slide

--- src/components/miniComponents/middleTopInto.jsx
+++ src/components/miniComponents/middleTopInto.jsx
@@ -1,4 +1,4 @@
-import bgs from "../../assets/Group 7159.png";
+import bgs from "../../assets/Group 71592.png";
 import keyboardImg from "../../assets/Девайсы premium брендов (посмотри какая из двух лучше) 1.png";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
 import {
@@ -31,12 +31,44 @@ const MiddleTopInto = () => {
           <SwiperSlide>
             <div className="w-full h-screen relative">
               <img src={bgs} alt="" className="w-full h-screen object-cover " />
+              <img
+                src={keyboardImg}
+                alt=""
+                className="w-250 absolute top-10 h-160 right-10"
+              />
+              <div className=" absolute top-60 left-20">
+                <p className="text-green-600  text-3xl pt-5 lg:text-6xl">
+                  FPS ИМЕЕТ ЗНАЧЕНИЕ
+                </p>
+                <p className="text-white pt-5 text-xl lg:text-xl lg:w-145">
+                  Компьютерный клуб построенный геймерами для геймеров. Каждая
+                  из наших игровых зон отвечает требованиям любых современных
+                  игровых проектов, позволяет добиваться высоких результатов и
+                  обходить соперников.
+                </p>
+              </div>
             </div>
           </SwiperSlide>
 
           <SwiperSlide>
-            <div className="w-full flex justify-center">
-              <img src={keyboardImg} alt="" className="w-full h-screen" />
+            <div className="w-full h-screen relative">
+              <img src={bgs} alt="" className="w-full h-screen object-cover " />
+
+              <div className=" absolute top-60 left-20">
+                <p className="text-white pt-5 text-xl lg:text-xl ">
+                  STANDART PX
+                </p>
+                <p className="text-green-600  text-3xl pt-5 lg:text-5xl lg:w-170 lg:leading-17">
+                  CPU INTEL CORE I5 4.6MHZ RAM 16GB 3200MHZ CL 15 RTX2070 8GB
+                  GDDR6
+                </p>
+              </div>
+              <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+              </div>
             </div>
           </SwiperSlide>
 
@@ -48,15 +80,6 @@ const MiddleTopInto = () => {
         </Swiper>
         <div className=" lg:absolute lg:z-10 lg:left-30">
           <img src={keyboardImg} alt="" className="w-full pt-20 lg:hidden" />
-          <p className="text-green-600  text-3xl pt-5 lg:text-6xl">
-            FPS ИМЕЕТ ЗНАЧЕНИЕ
-          </p>
-          <p className="text-white pt-5 text-xl lg:text-xl lg:w-145">
-            Компьютерный клуб построенный геймерами для геймеров. Каждая из
-            наших игровых зон отвечает требованиям любых современных игровых
-            проектов, позволяет добиваться высоких результатов и обходить
-            соперников.
-          </p>
         </div>
       </div>
     </div>
