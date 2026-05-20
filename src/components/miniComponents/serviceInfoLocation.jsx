aa25f5f6fb1c9c4f32c7bd0c723903608200a4c5
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-20 23:18:27 +0500

    services

--- src/components/miniComponents/serviceInfoLocation.jsx
+++ src/components/miniComponents/serviceInfoLocation.jsx
@@ -2,11 +2,11 @@
 const ServiceInfoLocation = () => {
   return (
    
-      <div className="p-5">
+      <div className="p-5 lg: absolute lg:top-240 lg:left-1/2 -translate-x-1/2 ">
         <p className="text-green-600 text-center font-black text-5xl pt-30">
           WHERE ARE WE LOCATED
         </p>
-        <p className="text-white text-center text-4xl pt-20">
+        <p className="text-white text-center text-4xl pt-20 lg:text-2xl lg:pt-5">
           МОСКВА, ЛОМОНОСОВСКИЙ ПРОСПЕКТ, 29К3
         </p>
       </div>
--- src/components/miniComponents/servicesInto.jsx
+++ src/components/miniComponents/servicesInto.jsx
@@ -5,18 +5,24 @@ import ServiceInfoLocation from './serviceInfoLocation';
 
 const ServicesInto = () => {
   return (
-    <div className="p-5">
-      <p className="text-green-600 text-center pt-20 font-black text-3xl">
+    <div className="p-5 lg: relative">
+      <p className="text-green-600 text-center pt-20 font-black text-3xl lg:text-start lg:text-xl lg:pl-20">
         SERVICES
       </p>
-      <img src={girl} alt="" className="w-full pt-20" />
-      <p className="text-green-600 text-3xl pt-10">НАШИ КОНТАКТЫ</p>
-      <p className="pt-10 text-white text-2xl">
-        Наши сотрудники всегда готовы помочь вам с выбором, оформить бронь и
-        предоставить лучший сервис
-      </p>
-      <p className=" text-white pt-20 text-center">+992 917-20-8222</p>
-      <div className=" flex justify-center items-center pt-5 gap-5">
+      <img
+        src={girl}
+        alt=""
+        className="w-full pt-20 lg:w-250 lg:absolute lg:right-20"
+      />
+      <div className=' lg:absolute lg:left-40 z-10 lg:w-120 lg:top-100'>
+        <p className="text-green-600 text-3xl pt-10 lg:text-5xl">НАШИ КОНТАКТЫ</p>
+        <p className="pt-10 text-white text-2xl lg:text-xl ">
+          Наши сотрудники всегда готовы помочь вам с выбором, оформить бронь и
+          предоставить лучший сервис
+        </p>
+        <p className=" text-white pt-20 text-center lg:text-start lg:text-4xl">+992 917-20-8222</p>
+      </div>
+      <div className=" flex justify-center items-center pt-5 gap-5 lg:absolute lg:top-190 lg:left-45">
         <Button
           icon={<InstagramOutlined className=" text-5xl!" />}
           className="w-20! h-20! rounded-3xl!"
@@ -30,7 +36,7 @@ const ServicesInto = () => {
           className="w-20! h-20! rounded-3xl!"
         />
       </div>
-<ServiceInfoLocation/>
+      <ServiceInfoLocation />
     </div>
   );
 }
