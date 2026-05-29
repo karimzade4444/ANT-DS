4a6bdd1d07162e0bc78678b1154d8017b58b7b7e
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-29 23:54:06 +0500

    component

--- src/components/middleBottomGames.jsx
+++ src/components/middleBottomGames.jsx
@@ -1,10 +1,10 @@
-
+import Games from "./miniComponents/games"
 
 const MiddleBottomGames = () => {
   return (
     <div className=" h-screen bg-black/80">
         <p className=" text-green-600 text-4xl font-extrabold text-center p-5">GAMES</p>
-
+       <Games/>
     </div>
   )
 }
