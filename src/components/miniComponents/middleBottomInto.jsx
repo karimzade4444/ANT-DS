d6a6dd9dd99e527ef505826f53b10ee12314777a
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-29 23:42:10 +0500

    favourite place

--- src/components/miniComponents/middleBottomInto.jsx
+++ src/components/miniComponents/middleBottomInto.jsx
@@ -4,7 +4,7 @@ const MiddleBottomInto = () => {
   return (
     <div className='p-5 pt-20 lg:relative'>
         <p className='text-green-600 font-black text-center text-2xl'>YOUR FAVORITE PLACES</p>
-        <img src={place} alt="" className='w-full pt-20 lg:w-150'/>
+        <img src={place} alt="" className='w-full pt-20 lg:w-150 lg:absolute lg:top-0'/>
         <p className='text-green-600 text-3xl pt-10'>PRIVATE PLAYSTATION 5 ROOM ДЛЯ ИЗЫСКАННЫХ ГЕЙМЕРОВ</p>
     </div>
   )
