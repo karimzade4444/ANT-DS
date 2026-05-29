6cb0a774b056b4e4684d54603e50aa0ffd86a417
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-29 23:39:34 +0500

    adress

--- src/components/miniComponents/serviceInfoLocation.jsx
+++ src/components/miniComponents/serviceInfoLocation.jsx
@@ -2,7 +2,7 @@
 const ServiceInfoLocation = () => {
   return (
    
-      <div className="p-5 lg: absolute lg:top-240 lg:left-1/2 -translate-x-1/2 ">
+      <div className="p-5 lg: absolute lg:top-240 lg:left-1/2 lg:-translate-x-1/2 ">
         <p className="text-green-600 text-center font-black text-5xl pt-30">
           WHERE ARE WE LOCATED
         </p>
