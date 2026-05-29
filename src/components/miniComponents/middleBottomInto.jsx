e03d130b7ce8fda5711d7b197cd9cd3efd1c7412
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-05-29 23:45:01 +0500

    text

--- src/components/miniComponents/middleBottomInto.jsx
+++ src/components/miniComponents/middleBottomInto.jsx
@@ -3,9 +3,9 @@ import place from '../../assets/15 1.png'
 const MiddleBottomInto = () => {
   return (
     <div className='p-5 pt-20 lg:relative'>
-        <p className='text-green-600 font-black text-center text-2xl'>YOUR FAVORITE PLACES</p>
-        <img src={place} alt="" className='w-full pt-20 lg:w-150 lg:absolute lg:top-0'/>
-        <p className='text-green-600 text-3xl pt-10'>PRIVATE PLAYSTATION 5 ROOM ДЛЯ ИЗЫСКАННЫХ ГЕЙМЕРОВ</p>
+        <p className='text-green-600 font-black text-center text-2xl lg:pl-150 lg:text-5xl'>YOUR FAVORITE PLACES</p>
+        <img src={place} alt="" className='w-full pt-20 lg:w-150 lg:absolute lg:top-0 '/>
+        <p className='text-green-600 text-3xl pt-10 lg:pl-180 lg:pt-20 lg:text-4xl'>PRIVATE PLAYSTATION 5 ROOM ДЛЯ ИЗЫСКАННЫХ ГЕЙМЕРОВ</p>
     </div>
   )
 }
