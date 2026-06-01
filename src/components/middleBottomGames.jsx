3226a01bd1cd66ee3ec33c01522ac22e5724358c
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-01 20:59:14 +0500

    search

--- src/components/middleBottomGames.jsx
+++ src/components/middleBottomGames.jsx
@@ -11,7 +11,7 @@ const MiddleBottomGames = ({
       <p className=" text-green-600 text-4xl font-extrabold text-center p-5">
         GAMES
       </p>
-      <div className=" flex justify-around items-center">
+      <div className=" flex justify-around items-center flex-wrap gap-7 p-5">
         <input type="search" name="" id="" className=" w-100 h-14 bg-white/60 rounded-2xl" />
         <button className=" w-60 h-14 bg-white rounded-2xl font-black cursor-pointer">Добавить новую игру</button>
       </div>
