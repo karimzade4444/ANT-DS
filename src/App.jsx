0339f25ba146bf667366d33138d24c504a5f40c9
Karimzoda Mustafo <karimzademustafa@gmail.com>
2026-06-01 20:30:32 +0500

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,16 @@ import Action from "./components/action";
 import ContactUs from "./components/contactUs";
 import Services from "./components/services";
 import Footertop from "./components/Footertop";
-import logotypes from ".//assets/Group 7169.png";
+import logotypes from "./assets/Group 7169.png"; // Исправил двойной слэш .//
 import MiddleBottomGames from "./components/middleBottomGames";
 
-  const [games, SetGames] = useState ([
+const App = () => {
+  // --- 1. ВСЕ ХУКИ И СТЕЙТЫ ДОЛЖНЫ БЫТЬ ТУТ, ВНУТРИ АРР ---
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const swiper = useSwiper();
+
+  const [games, setGames] = useState([
+    // Хорошая практика: маленькая буква в setGames
     {
       id: 1,
       image:
@@ -38,7 +44,6 @@ import MiddleBottomGames from "./components/middleBottomGames";
       platform: "PS5",
       genre: "Action",
     },
-
     {
       id: 2,
       image:
@@ -47,7 +52,6 @@ import MiddleBottomGames from "./components/middleBottomGames";
       platform: "PC",
       genre: "RPG",
     },
-
     {
       id: 3,
       image:
@@ -57,39 +61,51 @@ import MiddleBottomGames from "./components/middleBottomGames";
       genre: "Shooter",
     },
   ]);
+
+  // --- 2. CRUD ФУНКЦИИ ТОЖЕ ВНУТРИ КОМПОНЕНТА ---
   const createGames = (newImage, newTitle, newPlatform, newGenre) => {
-    const newItem = { id: Date.now(), image: newImage, title: newTitle, platform: newPlatform, genre: newGenre };
-    SetGames([...games, newItem]);
+    const newItem = {
+      id: Date.now(),
+      image: newImage,
+      title: newTitle,
+      platform: newPlatform,
+      genre: newGenre,
+    };
+    setGames([...games, newItem]);
   };
 
   const deleteGames = (idToDelete) => {
-  
     const updatedGames = games.filter((item) => item.id !== idToDelete);
-
-    SetGames(updatedGames);
+    setGames(updatedGames);
   };
-  const updateGames = (idToUpdate, updatedImage, updatedTitle, updatedPlatform, updatedGenre) => {
+
+  const updateGames = (
+    idToUpdate,
+    updatedImage,
+    updatedTitle,
+    updatedPlatform,
+    updatedGenre,
+  ) => {
     const updateddGames = games.map((item) => {
       if (item.id === idToUpdate) {
-      
-        return { ...item, image: updatedImage, title: updatedTitle, platform: updatedPlatform, genre: updatedGenre };
+        return {
+          ...item,
+          image: updatedImage,
+          title: updatedTitle,
+          platform: updatedPlatform,
+          genre: updatedGenre,
+        };
       }
-    
       return item;
     });
-
-    SetGames(updateddGames);
+    setGames(updateddGames);
   };
 
-
-const App = () => {
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const swiper = useSwiper();
   return (
     <>
-      <div className=" relative">
-        <div className=" fixed w-full left-0 top-0 z-100  bg-black/50">
-          <div className="  flex justify-evenly items-center gap p-5 gap-30">
+      <div className="relative">
+        <div className="fixed w-full left-0 top-0 z-100 bg-black/50">
+          <div className="flex justify-evenly items-center p-5 gap-30">
             <img src={logo} alt="" className="w-30" />
             <div className="flex justify-center items-center gap-3 lg:hidden">
               <Button
@@ -100,14 +116,19 @@ const App = () => {
               />
               <p className="text-white! text-2xl">Menu</p>
             </div>
-            <div className="hidden  text-white lg:flex justify-center items-center gap-10">
+            <div className="hidden text-white lg:flex justify-center items-center gap-10">
               <p>О нас</p>
               <p>Твои любимые места</p>
               <p>Bar & Hookah</p>
               <p>Акции</p>
               <p>Наши цены</p>
             </div>
-            <Button variant="filled" className=" rounded-3xl! bg-white/30! border-white/50! text-white! hidden! lg:block!">Связаться с нами</Button>
+            <Button
+              variant="filled"
+              className="rounded-3xl! bg-white/30! border-white/50! text-white! hidden! lg:block!"
+            >
+              Связаться с нами
+            </Button>
           </div>
         </div>
         <TopPosition />
@@ -115,7 +136,15 @@ const App = () => {
         <MiddleTop />
         <Middle />
         <MiddleBottom />
-        <MiddleBottomGames />
+
+        {/* --- 3. ПЕРЕДАЕМ ДАННЫЕ И ФУНКЦИИ В КОМПОНЕНТ ИГР ЧЕРЕЗ ПРОПСЫ --- */}
+        <MiddleBottomGames
+          games={games}
+          createGames={createGames}
+          deleteGames={deleteGames}
+          updateGames={updateGames}
+        />
+
         <Bar />
         <BarInfo />
         <Discount />
@@ -126,13 +155,13 @@ const App = () => {
 
         <Drawer
           open={openDrawer}
-          className=" text-white! bg-gray-700!"
+          className="text-white! bg-gray-700!"
           onClose={() => {
             setOpenDrawer(false);
           }}
         >
-          <div className="h-screen  pt-20">
-            <div className=" flex justify-center items-center flex-col leading-20 text-white text-3xl">
+          <div className="h-screen pt-20">
+            <div className="flex justify-center items-center flex-col leading-20 text-white text-3xl">
               <img src={logotypes} alt="" className="w-50" />
               <p className="pt-10">About us</p>
               <p>Your favorite places</p>
@@ -144,67 +173,6 @@ const App = () => {
             </p>
           </div>
         </Drawer>
-
-        {/* <h1>GOSU</h1>
-        <div className="lg:flex gap-5 justify-center items-center hidden">
-          <p>О нас</p>
-          <p>Твои любимые места</p>
-          <p>Bar&Hookah</p>
-          <p>Bar&Hookah</p>
-          <p>Bar&Hookah</p>
-        </div>
-        <Button
-          className="hidden! lg:block!"
-          shape="round"
-          color="default"
-          variant="filled"
-        >
-          Связаться с нами
-        </Button>
-        <Button
-          className="lg:hidden!"
-          icon={<MenuOutlined />}
-          onClick={() => {
-            setOpenDrawer(true);
-          }}
-        />
-        <Drawer
-          open={openDrawer}
-          onClose={() => {
-            setOpenDrawer(false);
-          }}
-        >
-          <div className="flex flex-col gap-5 justify-center items-center">
-            <p>О нас</p>
-            <p>Твои любимые места</p>
-            <p>Bar&Hookah</p>
-            <p>Bar&Hookah</p>
-            <p>Bar&Hookah</p>
-          </div>
-        </Drawer>
-      </div>
-
-      <div className="w-1/2">
-        <Swiper
-          slidesPerView={1}
-          spaceBetween={100}
-          onSlideChange={() => console.log("slide change")}
-          onSwiper={(swiper) => console.log(swiper)}
-          modules={[Pagination,Autoplay]}
-          pagination={{ clickable: true }}
-      
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
-        >
-          <SwiperSlide>
-            <div className="w-full h-50 bg-blue-600"></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-full h-50 bg-red-600"></div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="w-full h-50 bg-green-600"></div>
-          </SwiperSlide>
-        </Swiper> */}
       </div>
     </>
   );
--- src/components/middleBottomGames.jsx
+++ src/components/middleBottomGames.jsx
@@ -1,12 +1,19 @@
 import Games from "./miniComponents/games"
 
-const MiddleBottomGames = () => {
+const MiddleBottomGames = ({
+  games,
+  deleteGames,
+  createGames,
+  updateGames,
+}) => {
   return (
     <div className=" h-screen bg-black/80">
-        <p className=" text-green-600 text-4xl font-extrabold text-center p-5">GAMES</p>
-       <Games/>
+      <p className=" text-green-600 text-4xl font-extrabold text-center p-5">
+        GAMES
+      </p>
+      <Games />
     </div>
-  )
-}
+  );
+};
 
 export default MiddleBottomGames
